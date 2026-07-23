3719b2ddb0708dc3cb293bd23c25a49fd6483cf9
kusuma <kusumacr19@gmail.com>
2026-07-23 16:03:56 +0530

    1

--- src/test/java/com/jsp/book/BookMyTicketApplicationTests.java
+++ src/test/java/com/jsp/book/BookMyTicketApplicationTests.java
@@ -1,8 +1,12 @@
 package com.jsp.book;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookMyTicketApplicationTests {
+    @Test
+   void contextLoads() {
+   }
 
 }
